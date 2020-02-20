5f988c85bb19b24c43c45ded57f5852cca76c356
susmitha <susmithavs49@gmail.com>
2020-02-20 00:27:42 -0800

    set layout and create model file

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -16,12 +16,40 @@ AppAsset::register($this);
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
-    <meta charset="<?= Yii::$app->charset ?>">
+<meta charset="UTF-8">
+<meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
+    
+   
+
+    <!-- Title Page-->
+    <title>Au Register Forms by Colorlib</title>
+
+    <!-- Icons font CSS-->
+    <link href="<?php echo \Yii::$app->request->BaseUrl?>/signuptheme/vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="<?php echo \Yii::$app->request->BaseUrl?>/signuptheme/vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <!-- Font special for pages-->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+
+    <!-- Vendor CSS-->
+    <link href="<?php echo \Yii::$app->request->BaseUrl?>/signuptheme/vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="<?php echo \Yii::$app->request->BaseUrl?>/signuptheme/vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="<?php echo \Yii::$app->request->BaseUrl?>/signuptheme/css/main.css" rel="stylesheet" media="all">
+    <script src="<?php echo \Yii::$app->request->BaseUrl?>/signuptheme/vendor/jquery/jquery.min.js"></script>
+    <!-- Vendor JS-->
+    <script src="<?php echo \Yii::$app->request->BaseUrl?>/signuptheme/vendor/select2/select2.min.js"></script>
+    <script src="<?php echo \Yii::$app->request->BaseUrl?>/signuptheme/vendor/datepicker/moment.min.js"></script>
+    <script src="<?php echo \Yii::$app->request->BaseUrl?>/signuptheme/vendor/datepicker/daterangepicker.js"></script>
+
+    <!-- Main JS-->
+    <script src="<?php echo \Yii::$app->request->BaseUrl?>/signuptheme/js/global.js"></script>
+
 </head>
 <body>
 <?php $this->beginBody() ?>
@@ -29,30 +57,34 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
+        // 'brandLabel' => Yii::$app->name,
+        // 'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
         ],
     ]);
     $menuItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
+        // ['label' => 'Create Leads', 'url' => ['/lead/create']],
+
+
+      
+        // ['label' => 'About', 'url' => ['/site/about']],
+        // ['label' => 'Contact', 'url' => ['/site/contact']],
     ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-    } else {
-        $menuItems[] = '<li>'
-            . Html::beginForm(['/site/logout'], 'post')
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout']
-            )
-            . Html::endForm()
-            . '</li>';
-    }
+    // if (Yii::$app->user->isGuest) {
+    //     $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
+    //     $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+    // } else {
+    //     $menuItems[] = '<li>'
+    //         . Html::beginForm(['/site/logout'], 'post')
+    //         . Html::submitButton(
+    //             'Logout (' . Yii::$app->user->identity->username . ')',
+    //             ['class' => 'btn btn-link logout']
+    //         )
+    //         . Html::endForm()
+    //         . '</li>';
+    // }
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => $menuItems,
